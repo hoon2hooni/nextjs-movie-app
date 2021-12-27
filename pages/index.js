077775fb3cdb77cb2001dad27e18149737f6d130
hoon2hooni <sanghoon.engineer@gmail.com>
2021-12-27 09:55:09 +0900

    feat: add counter feature at home

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function Home() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h1> Number is {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>click me</button>
+    </div>
+  );
+}
