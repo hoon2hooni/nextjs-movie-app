d4c770237f2b17b72c6ca5db891a51b4093630fd
hoon2hooni <sanghoon.engineer@gmail.com>
2021-12-29 10:40:15 +0900

    imp: rewrite endpoint to hide api key with configuring next.config file

--- components/Movies/Movies.jsx
+++ components/Movies/Movies.jsx
@@ -1,26 +1,15 @@
-import { stringify } from "query-string";
 import { useEffect, useState } from "react";
 import styles from "./Movies.module.css";
-
-const BASE_URL = "https://api.themoviedb.org/3";
-const path = "/movie/popular";
-
-const queryString = stringify({
-  api_key: process.env.NEXT_PUBLIC_API_KEY,
-  language: "en-US",
-});
-
-const getPopularMoviesEndPoint = `${BASE_URL}${path}?${queryString}`;
 
 export default function Movies() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     (async () => {
-      const response = await fetch(getPopularMoviesEndPoint);
+      const response = await fetch("/api/movies");
       const { results = [] } = await response.json();
       setMovies(results);
     })();
-  }, [BASE_URL]);
+  }, []);
 
   if (movies.length === 0) {
     return <h1>is Loading</h1>;
@@ -30,9 +19,12 @@ export default function Movies() {
     <div className={styles.container}>
       {movies.map(({ adult, title, vote_average }) => (
         <div className={styles.item}>
-          <h1>{title}</h1>
-          <h2>{adult ? "성인" : "전 연령 관람 가능"}</h2>
-          <h3>평점 : {vote_average} </h3>
+          <img src={""} />
+          <div>
+            <h1>{title}</h1>
+            <h2>{adult ? "성인" : "전 연령 관람 가능"}</h2>
+            <h3>평점 : {vote_average} </h3>
+          </div>
         </div>
       ))}
     </div>
--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
+const BASE_URL = "https://api.themoviedb.org/3";
+const path = "/movie/popular";
+const getPopularMoviesEndPoint = `${BASE_URL}${path}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`;
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: getPopularMoviesEndPoint,
+      },
+    ];
+  },
+};
